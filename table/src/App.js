3979309664cf9c67100b13660258bab7efda70db
sdon <sdonchenko@outlook.com>
2021-08-22 11:30:24 +0300

    respons to api

--- table/src/App.js
+++ table/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Table from "./componets/Table";
+import React from 'react'
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    let response = fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json()).then(commits => console.log(commits))
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //   if (response.ok) { // если HTTP-статус в диапазоне 200-299
+  //   // получаем тело ответа (см. про этот метод ниже)
+  //   let json = await response.json();
+  //   } else {
+  //   alert("Ошибка HTTP: " + response.status);
+  // }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Table/>
+      </div>
+    );
+  }
 }
 
 export default App;
